562f5e9682f7a83d6904ef78dbec6f90b23549c6
shenalachintha <achinthas666@gmail.com>
2025-11-17 00:11:32 +0530

    new commit

--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -1,11 +1,13 @@
+
 const Pizza = (props) => {
+   
     return(
         <div className="Pizza">
-            <h1>{props.name}</h1>
+            <h1>{props.name} </h1>
             <p>{props.description}</p>
             <img src={props.image} alt={props.name} />
         </div>
     )
 }
-export { Pizza };
+
 export default Pizza;
