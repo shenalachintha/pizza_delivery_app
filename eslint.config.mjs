dcb3a85e005b0a7bcaf3f8ec97a3af927f5eac0e
shenalachintha <achinthas666@gmail.com>
2025-11-16 13:18:15 +0530

    new commit

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import js from "@eslint/js";
+import globals from "globals";
+import prettier from "eslint-config-prettier";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node, React: 'readonly', ReactDOM: 'readonly' },
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  },
+  prettier,
+];
