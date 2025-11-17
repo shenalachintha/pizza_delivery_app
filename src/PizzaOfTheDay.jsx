8291530ff5463059d59fef0ee0bda8bee5eeef2f
shenalachintha <achinthas666@gmail.com>
2025-11-17 18:46:51 +0530

    new commit

--- src/PizzaOfTheDay.jsx
+++ src/PizzaOfTheDay.jsx
@@ -12,20 +12,23 @@ const PizzaOfTheDay = () => {
     }
     return(
         <div className="pizza-of-the-day">
-            <h2>Pizza of the Day </h2>
-            <div className='pizza-of-the-day-info'>
-                <h3>{pizzaOfTheDay.name}</h3>
-            <p>{pizzaOfTheDay.description}</p>
-            <p className="pizza-of-the-day-price">
+      <h2>Pizza of the Day</h2>
+      <div>
+        <div className="pizza-of-the-day-info">
+          <h3>{pizzaOfTheDay.name}</h3>
+          <p>{pizzaOfTheDay.description}</p>
+          <p className="pizza-of-the-day-price">
             From: <span>{intl.format(pizzaOfTheDay.sizes.S)}</span>
-          </p>   
-            </div>
-            <img 
-            className='pizza-of-the-day-image'
-            src={pizzaOfTheDay.image} 
-            alt={pizzaOfTheDay.name} />
+          </p>
         </div>
-    )
-
+        <img
+          className="pizza-of-the-day-image"
+          src={pizzaOfTheDay.image}
+          alt={pizzaOfTheDay.name}
+        />
+      </div>
+    </div>
+  );
 }
+
 export default PizzaOfTheDay
