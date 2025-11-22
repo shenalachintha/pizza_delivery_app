9d6adc4570cb1f701d48d8f48942b32a23051ce6
shenalachintha <achinthas666@gmail.com>
2025-11-22 09:04:01 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,22 @@ import { createRoot } from "react-dom/client";
 import Order from "./Order.jsx";
 import PizzaOfTheDay from "./PizzaOfTheDay.jsx";
 import Header from "./Header.jsx";
+import { CartContext } from "./contexts.jsx";
+import { StrictMode, useState } from "react";
 
 const App = () => {
+  const cartHook=useState([]);
   return (
-    <div>
-      <Header/>
+    <StrictMode>
+      <CartContext.Provider value={cartHook}>
+        <div>
+      <Header />
      <Order />
      <PizzaOfTheDay />
     </div>
-  );
+  </CartContext.Provider>
+    </StrictMode>
+  )
 };
 
 const container = document.getElementById("root");
