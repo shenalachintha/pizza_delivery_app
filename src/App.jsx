5c1da2418e8253e1c7c6a09ea1594ff8f4baf3d6
shenalachintha <achinthas666@gmail.com>
2025-11-22 10:15:38 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 import { createRoot } from "react-dom/client";
-import Order from "./Order.jsx";
-import PizzaOfTheDay from "./PizzaOfTheDay.jsx";
-import Header from "./Header.jsx";
-import { CartContext } from "./contexts.jsx";
-import { StrictMode, useState } from "react";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+import { StrictMode} from "react";
+
+
+const router=createRouter({
+  routeTree,
+});
 
 const App = () => {
-  const cartHook=useState([]);
   return (
     <StrictMode>
-      <CartContext.Provider value={cartHook}>
-        <div>
-      <Header />
-     <Order />
-     <PizzaOfTheDay />
-    </div>
-  </CartContext.Provider>
+    <RouterProvider router={router} />
     </StrictMode>
   )
 };
